833e1a1f96e37df9431b8bd58b2ee8fce7aaddda
Paul <paulberthelot10@gmail.com>
2020-02-18 11:11:39 +0100

    login2

--- login.php
+++ login.php
@@ -17,7 +17,7 @@
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <!------ Include the above in your HEAD tag ---------->
+    <!------ Framewwork ---------->
     
     <div class="wrapper fadeInDown">
       <div id="formContent">
@@ -29,34 +29,39 @@
         </div>
     
         <!-- Login Form -->
-        <form>
+        <form method="post" action="https://databases-auth.000webhost.com/sql.php?server=1&db=id12615311_contact&table=formulaire&pos=0">
           <input type="text" id="login" class="fadeIn second" name="login" placeholder="login">
-          <input type="text" id="password" class="fadeIn third" name="login" placeholder="password">
+          <input type="text" id="password" class="fadeIn third" name="password" placeholder="password">
           <input type="submit" class="fadeIn fourth" value="Log In">
         </form>
     
         <!-- Remind Passowrd -->
         <div id="formFooter">
-          <a class="underlineHover" href="#">Forgot Password?</a>
+          <a class="underlineHover" href="index.html">Retourner à l'accueil</a>
         </div>
     
       </div>
     </div>
 
 
+<?php 
 
+if($_POST){
+    $Login = $_POST['login'];
+    $Password = $_POST['password'];
+   
+    if (!empty($Login) AND !empty($Password) )
+    {if (($Login == "admin") AND ($Password =="root")) {
+      echo "<href: 'https://databases-auth.000webhost.com/sql.php?server=1&db=id12615311_contact&table=formulaire&pos=0'>";
+    }
+    }
+     else {
+      echo "Erreur";
+     };
 
+    };
+?>  
 
-
-
-
-
-
-
-
-
-
-    
 
 </body>
 
